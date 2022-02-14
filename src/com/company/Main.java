c1fc985f8a1084aead43e70701d30a4005b8daf8
Karri Wright <karriwright@Karris-MacBook-Pro.local>
2022-02-14 15:44:20 -0500

    Add inital Repo

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int grade = 110;
+         if(grade < 60) {
+            System.out.println("You failed");
+         } else if (grade < 70) {
+            System.out.println("You got a D");
+        } else if (grade < 80) {
+            System.out.println("You got a C");
+        } else if (grade < 80) {
+            System.out.println("You got a B");
+        } else {
+            System.out.println("You got an A");
+    }}}
+
